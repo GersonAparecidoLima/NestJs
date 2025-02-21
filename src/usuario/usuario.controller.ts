4b49b658ca7914b9c0d5f3d5198c6f90cb95681c
Gerson Aparecido <gerson.lima@yduqs.com.br>
2025-02-21 18:17:58 -0300

    Criando DTO de listagem

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -3,6 +3,7 @@ import { UsuarioRepository } from './usuario.repository';
 import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
 import { UsuarioEntity } from './usuario.entity';
 import { v4 as uuid } from 'uuid';
+import { ListaUsuarioDTO } from './ListaUsuario.dto';
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -10,27 +11,37 @@ export class UsuarioController {
      //private usuarioRepository = new UsuarioRepository()
      constructor(private usuarioRepository: UsuarioRepository) {}
 
+
+
      @Post()
      async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
-          const usuarioEntity = new UsuarioEntity();
-          
-          usuarioEntity.email = dadosDoUsuario.email;
-          usuarioEntity.senha = dadosDoUsuario.senha;
-          usuarioEntity.nome = dadosDoUsuario.nome;
-          usuarioEntity.id = uuid();
+     const usuarioEntity = new UsuarioEntity();
+     usuarioEntity.email = dadosDoUsuario.email;
+     usuarioEntity.senha = dadosDoUsuario.senha;
+     usuarioEntity.nome = dadosDoUsuario.nome;
+     usuarioEntity.id = uuid();
 
+     this.usuarioRepository.salvar(usuarioEntity);
 
-          this.usuarioRepository.salvar(usuarioEntity);
           return {
-               id: usuarioEntity.id,
-               mensagem: 'usuário criado com sucesso'
-           };
+               usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome),
+               mensagem: 'usuário criado com sucesso',
+          };
      }
 
  
 
      @Get()
-    async listUsuarios() {
-    return this.usuarioRepository.listar()
-    }
+     async listUsuarios() {
+         const usuariosSalvos = await this.usuarioRepository.listar();
+         const usuariosLista = usuariosSalvos.map(
+             usuario => new ListaUsuarioDTO(
+                 usuario.id,
+                 usuario.nome
+             )
+         );
+     
+         return usuariosLista;
+     }
+
 }

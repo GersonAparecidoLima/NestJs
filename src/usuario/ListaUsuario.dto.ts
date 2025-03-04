618c885ad12fef77e48cc73c2f362afb2a2e1b89
Gerson Aparecido <gerson.lima@yduqs.com.br>
2025-03-04 14:25:59 -0300

    Final

--- src/usuario/ListaUsuario.dto.ts
+++ src/usuario/ListaUsuario.dto.ts
@@ -1,6 +1,13 @@
+
+export class ListaUsuarioDTO {
+    constructor(readonly id: string, readonly nome: string, readonly email:string) {}
+  }
+
+/*
 export class ListaUsuarioDTO {
     constructor(
           readonly id: string,
           readonly nome: string
       ) {}
   }
+      */
---
+++ src/usuario/Service/usuarioService.ts
@@ -0,0 +1,41 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { UsuarioRepository } from "../usuario.repository";
+import { CriaUsuarioDTO } from "../dto/CriaUsuario.dto";
+import { UsuarioEntity } from "../usuario.entity";
+import { randomUUID } from "crypto";
+
+@Injectable()
+export class UsuarioService {
+  constructor(private readonly usuarioRepository: UsuarioRepository) {}
+
+  async criar(dadosUsuario: CriaUsuarioDTO): Promise<UsuarioEntity> {
+   
+    const usuario = new UsuarioEntity();
+    
+    // Gerando o ID automaticamente
+    usuario.id = randomUUID();  
+    usuario.nome = dadosUsuario.nome;
+    usuario.email = dadosUsuario.email;
+    usuario.senha = dadosUsuario.senha;
+
+    // Salvando o usuário no repositório
+    return this.usuarioRepository.salvar(usuario);
+  }
+
+  async listar() {
+    return this.usuarioRepository.listar();
+  }
+
+  async atualizar(id: string, dadosDeAtualizacao: Partial<UsuarioEntity>) {
+    if (Object.keys(dadosDeAtualizacao).length === 0) {
+      throw new BadRequestException('Dados de atualização não podem estar vazios.');
+    }
+
+    return this.usuarioRepository.atualiza(id, dadosDeAtualizacao);
+  }
+
+  async remover(id: string) {
+    return this.usuarioRepository.remove(id);
+  }
+
+}
